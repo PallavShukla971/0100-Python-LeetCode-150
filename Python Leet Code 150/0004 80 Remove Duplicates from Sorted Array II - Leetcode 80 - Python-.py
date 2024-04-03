4bf6ed5c69b27b942a8791ba4b671a7e1feaac66
Pallav Shukla <38599912+PallavShukla971@users.noreply.github.com>
2024-04-04 00:33:00 +0100

    Update 0004 80 Remove Duplicates from Sorted Array II - Leetcode 80 - Python-.py
    0004 completed

--- Python Leet Code 150/0004 80 Remove Duplicates from Sorted Array II - Leetcode 80 - Python-.py
+++ Python Leet Code 150/0004 80 Remove Duplicates from Sorted Array II - Leetcode 80 - Python-.py
@@ -60,3 +60,47 @@ Remove Duplicates from Sorted Array
 
 
 """
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l, r = 0, 0
+
+        while r < len(nums):
+            count = 1
+            while r + 1 < len(nums) and nums[r] == nums[r + 1]:
+                r += 1
+                count += 1
+            
+            for i in range(min(2, count)):
+                nums[l] = nums[r]
+                l += 1
+                
+            r += 1
+
+        return l
+
+"""
+another solution as well:
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+        
+        # Initialize two pointers
+        slow = 2
+        for fast in range(2, len(nums)):
+            # If the current element is different from the two elements before it
+            if nums[fast] != nums[slow - 2]:
+                # Copy the current element to the next available position
+                nums[slow] = nums[fast]
+                # Move the slow pointer forward
+                slow += 1
+                
+        return slow
+
+
+"""
