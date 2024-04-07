3ac63bc6a783ce6151d779003816d18a2cdd57f2
Pallav Shukla <38599912+PallavShukla971@users.noreply.github.com>
2024-04-07 23:42:34 +0100

    Update 0002_Remove_Element_.py

--- Python Leet Code 150/0002_Remove_Element_.py
+++ Python Leet Code 150/0002_Remove_Element_.py
@@ -1,5 +1,5 @@
 """ 
-27. Remove Element.
+27. Remove Element..
 
 Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
