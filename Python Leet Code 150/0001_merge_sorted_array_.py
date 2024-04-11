85fb2eb482d104d012b2cf9f7d3e318dd1bccfac
Pallav Shukla <38599912+PallavShukla971@users.noreply.github.com>
2024-04-11 15:06:27 +0100

    Update 0001_merge_sorted_array_.py
    updated

--- Python Leet Code 150/0001_merge_sorted_array_.py
+++ Python Leet Code 150/0001_merge_sorted_array_.py
@@ -8,7 +8,7 @@ The final sorted array should not be returned by the function, but instead be st
  
 
 Example 1:
-
+.
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
